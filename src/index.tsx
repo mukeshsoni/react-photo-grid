4db17fd2b02c310e5ef39f2454bf8ab8302b80bc
Mukesh Soni <mukeshsoni@gmail.com>
2020-05-02 19:38:38 +0530

    remove lodash.throttle since it not used at all. change index.js filename to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
-import React, { Component } from "react";
+import * as React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
-import throttle from "lodash.throttle";
 
 // TODO - element resize event is not working
 var imageElements = [];
